f722376858a7d67b9aa80de441518599a3b57ce9
Coke <tao.wang@davincimotor.com>
2023-08-23 17:31:59 +0800

    fix: bug fix

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue"
 import { MagicStick, FullScreen } from "@element-plus/icons-vue"
 import { ElDialog } from "element-plus"
+import {Sidebar} from "@/layouts/components";
 
 const input = ref("")
 const dialogTableVisible = ref(false)
@@ -12,46 +13,16 @@ const clickButton = () => {
 
 <template>
   <div style="margin: 10px">
-    <el-dialog v-model="dialogTableVisible" title="魔法变量">
-      <el-collapse v-model="activeName" accordion>
-        <el-collapse-item title="Consistency" name="1">
-          <div>
-            Consistent with real life: in line with the process and logic of real life, and comply with languages and
-            habits that the users are used to;
-          </div>
-          <div>
-            Consistent within interface: all elements should be consistent, such as: design style, icons and texts,
-            position of elements, etc.
-          </div>
-        </el-collapse-item>
-        <el-collapse-item title="Feedback" name="2">
-          <div>
-            Operation feedback: enable the users to clearly perceive their operations by style updates and interactive
-            effects;
-          </div>
-          <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
-        </el-collapse-item>
-        <el-collapse-item title="Efficiency" name="3">
-          <div>Simplify the process: keep operating process simple and intuitive;</div>
-          <div>
-            Definite and clear: enunciate your intentions clearly so that the users can quickly understand and make
-            decisions;
-          </div>
-          <div>
-            Easy to identify: the interface should be straightforward, which helps the users to identify and frees them
-            from memorizing and recalling.
-          </div>
-        </el-collapse-item>
-        <el-collapse-item title="Controllability" name="4">
-          <div>
-            Decision making: giving advices about operations is acceptable, but do not make decisions for the users;
-          </div>
-          <div>
-            Controlled consequences: users should be granted the freedom to operate, including canceling, aborting or
-            terminating current operation.
-          </div>
-        </el-collapse-item>
-      </el-collapse>
+    <el-dialog v-model="dialogTableVisible" title="魔法变量" width="70%">
+      <el-row>
+        <el-col :span="8">
+          <el-scrollbar wrap-class="scrollbar-wrapper">
+            <Sidebar style="height: 200px" class="sidebar-container" />
+          </el-scrollbar>
+        </el-col>
+        <el-col :span="8"><div class="section">Test</div></el-col>
+        <el-col :span="8"><div class="section">Test</div></el-col>
+      </el-row>
     </el-dialog>
     <el-input v-model="input" class="w-50 m-2" style="width: 150px" placeholder="Pick a date">
       <template #suffix>
@@ -69,4 +40,16 @@ const clickButton = () => {
 :deep(.el-input__wrapper) {
   padding: 1px 3px 1px 11px;
 }
+.container {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.section {
+  flex: 1;
+  border: 1px solid #000;
+  padding: 20px;
+}
+
 </style>
